de7a53a4b70c632351901df20b22e9de2d6ff240
ZequinhGit <jr040405@gmail.com>
2025-10-22 19:59:02 -0300

    Versão final pronta para entrega

--- html/wp-config.php
+++ html/wp-config.php
@@ -1,51 +1,21 @@
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://wordpress.org/support/article/editing-wp-config-php/
- *
- * @package WordPress
- */
+// ===== Banco =====
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wpuser');
+define('DB_PASSWORD', 'wppass');
+define('DB_HOST', 'db:3306');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress');
+// ===== URL pública atrás do Nginx =====
+define('WP_HOME',    'http://localhost:8080');
+define('WP_SITEURL', 'http://localhost:8080');
 
-/** MySQL database username */
-define( 'DB_USER', 'wpuser');
+// ===== Cookies (evita domínio preso) =====
+define('COOKIE_DOMAIN', false);
 
-/** MySQL database password */
-define( 'DB_PASSWORD', 'wppass');
-
-/** MySQL hostname */
-define( 'DB_HOST', 'db:3306');
-
-/** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '');
-
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
+// ===== AUTH KEYS & SALTS (iguais em todos os nós) =====
+// Use UM conjunto e mantenha igual pra todos.
 define( 'AUTH_KEY',         'e5eddf9664d1b8bdc46dd250cbfa969833f652c1');
 define( 'SECURE_AUTH_KEY',  'c15b632dee236cf8606582776eabc7eb8950b0b2');
 define( 'LOGGED_IN_KEY',    '097b20afe91127685b778cfe82a4b069abe7cc03');
@@ -55,42 +25,15 @@ define( 'SECURE_AUTH_SALT', '41594bca8c4a7c0dc5da8aadc7e185e9f35c83a0');
 define( 'LOGGED_IN_SALT',   '1893be093447bfcd1fb7e65f94e8bff23dbc9cc3');
 define( 'NONCE_SALT',       '4896cb5edc6ae5f0525034438287d5d3297cc32e');
 
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix = 'wp_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
- */
-define( 'WP_DEBUG', false );
-
-// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
-// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+// Proxy: preserve HTTPS se vier do Nginx
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-	$_SERVER['HTTPS'] = 'on';
+  $_SERVER['HTTPS'] = 'on';
 }
 
-/* That's all, stop editing! Happy publishing. */
+// ===== Prefixo e debug =====
+$table_prefix = 'wp_';
+define('WP_DEBUG', false);
 
-/** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
-}
-
-/** Sets up WordPress vars and included files. */
+// ===== Bootstrap =====
+if (!defined('ABSPATH')) define('ABSPATH', __DIR__ . '/');
 require_once ABSPATH . 'wp-settings.php';
--- locustfile.py
+++ locustfile.py
@@ -1,12 +1,21 @@
 from locust import HttpUser, task, between
 
+# TROQUE pelos IDs reais: /?p=<ID>
+POST_IMG_1MB   = "/?p=8"  # imagem ~1 MB
+POST_TEXTO_400 = "/?p=11"  # texto ~400 KB
+POST_IMG_300KB = "/?p=5"  # imagem ~300 KB
+
 class WordPressUser(HttpUser):
     wait_time = between(1, 3)
 
-    @task(10)
-    def visit_homepage(self):
-        self.client.get("/") # Simple GET
-
     @task(3)
-    def visit_sample_post(self):
-        self.client.get("/?p=1") # Simple GET
+    def post_imagem_grande(self):
+        self.client.get(POST_IMG_1MB, name="Post imagem 1MB")
+
+    @task(2)
+    def post_texto_longo(self):
+        self.client.get(POST_TEXTO_400, name="Post texto 400KB")
+
+    @task(2)
+    def post_imagem_leve(self):
+        self.client.get(POST_IMG_300KB, name="Post imagem 300KB")
